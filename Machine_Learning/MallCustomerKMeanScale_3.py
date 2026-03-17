b20beb64b174c23da6149638e3864908a2560289
Aditya Sanap <adityasanap2001@gmail.com>
2026-03-17 18:59:48 +0530

    K mean cluster case study

---
+++ Machine_Learning/MallCustomerKMeanScale_3.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
+
+def main():
+    #-----------------------------------------------------------------------------------------
+    # Step 1 : Load the dataset
+    #-----------------------------------------------------------------------------------------
+    
+    print("Step 1 : Load the dataset")
+    df = pd.read_csv("Mall_Customers.csv")
+    
+    print("\nFirst few records ")
+    print(df.head())
+    
+    print("\nShape of dataset : ")
+    print(df.shape)
+    
+    print("\nMissing Values : ")
+    print(df.isnull().sum())
+    
+    #-----------------------------------------------------------------------------------------
+    # Step 2 : Select Features (Independent)
+    #-----------------------------------------------------------------------------------------
+    
+    print("Step 2 : Select Features (Independent)")
+    
+    X = df[["AnnualIncome", "SpendingScore"]]
+    
+    print("\nSelected features : ")
+    print(X.head())
+    
+    print("Shape of selected features : ")
+    print(X.shape)
+    
+    #-----------------------------------------------------------------------------------------
+    # Step 3 : Scale the data
+    #-----------------------------------------------------------------------------------------
+    
+    print("Step 3 : Scale the data")
+    
+    scaler = StandardScaler()
+    X_scaled = scaler.fit_transform(X)
+    
+    print("Data after scaling : ")
+    print(X_scaled[:5])
+    
+    
+    
+if __name__ == "__main__":
+    main()
